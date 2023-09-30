de8b1a22bf501c43fde45ef4c8273bcf48b31b07
Ace14723 <144862076+Ace14723@users.noreply.github.com>
2023-09-30 16:11:52 -0500

    Update Email_search.py

--- Email_search.py
+++ Email_search.py
@@ -30,7 +30,7 @@ def search_email():
     chrome_options = Options()
     # Add any desired options to chrome_options here, e.g.
     chrome_options.add_argument("--headless")
-    driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", options=chrome_options)
+    driver = webdriver.Remote(command_executor="http://127.0.0.1:4444/wd/hub", options=chrome_options)
     
     driver.get('https://pplsearch.amfam.com/pplsearch/')
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "optAttribute")))
