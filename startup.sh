5f0fa8a9f6a266360430718258826c634b31d6d4
Ace14723 <144862076+Ace14723@users.noreply.github.com>
2023-09-30 13:22:46 -0500

    Delete startup.sh

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Start the Selenium standalone server in the background
-/opt/bin/start-xvfb.sh &
-/opt/bin/start-selenium-standalone.sh &
-# Give it a moment to start
-sleep 5
-# Start the Flask application
-python ./Email_search.py
